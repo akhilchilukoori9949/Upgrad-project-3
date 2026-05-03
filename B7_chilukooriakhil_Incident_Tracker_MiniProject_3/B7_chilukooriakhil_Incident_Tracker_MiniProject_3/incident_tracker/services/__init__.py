1de59b99f67285602124e0fc5a7ef8b18e245ff1
akhilchilukoori <akhilbisiness@gmail.com>
2026-05-03 19:54:51 +0530

    Delete B7_chilukooriakhil_Incident_Tracker_MiniProject_3/B7_chilukooriakhil_Incident_Tracker_MiniProject_3 directory

--- B7_chilukooriakhil_Incident_Tracker_MiniProject_3/B7_chilukooriakhil_Incident_Tracker_MiniProject_3/incident_tracker/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .azure_boards import AzureBoardsService
-from .jira import JiraService
-from .servicenow import ServiceNowService
-
-__all__ = ["AzureBoardsService", "JiraService", "ServiceNowService"]
--- B7_chilukooriakhil_Incident_Tracker_MiniProject_3/B7_chilukooriakhil_Incident_Tracker_MiniProject_3/incident_tracker/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .classifier import detect_severity, detect_type
-from .decorators import log_call, retry
-
-__all__ = ["detect_severity", "detect_type", "log_call", "retry"]
